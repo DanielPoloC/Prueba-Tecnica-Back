6e357c32795b63f43a51d585b02873d2e397fcb4
dpolo-jamar <44912504+DanielPoloC@users.noreply.github.com>
2022-03-31 04:47:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.get("/v1/empleados/obtener", async (req, res) => {
         TO_CHAR(fecha_ingreso, 'DD/MM/YYYY') fecha_ingreso,
         nombre_area,
         estado,
-        TO_CHAR(fecha_registro, 'DD/MM/YYYY HH:MM:SS') fecha_registro,
-        TO_CHAR(fecha_edicion, 'DD/MM/YYYY'  HH:MM:SS) fecha_edicion
+        TO_CHAR(fecha_registro, 'DD/MM/YYYY hh:mm:ss') fecha_registro,
+        TO_CHAR(fecha_edicion, 'DD/MM/YYYY  hh:mm:ss') fecha_edicion
       FROM empleado 
       WHERE estado = 'Activo'
     `
